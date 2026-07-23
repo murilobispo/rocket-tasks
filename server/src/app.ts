64ec33b20fd15fbfbde66cdca059ecd59c3c3fe5
Murilo Bispo <murilooliveirabispo90@gmail.com>
2026-07-23 02:49:31 -0300

    chore: add helmet security middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { authRoutes } from '@/routes/auth.routes'
+import helmet from 'helmet'
 
 const app = express()
 
 app.use(express.json())
+app.use(helmet())
 
 app.get('/', (req, res) => {
   res.status(200).json({message: 'Rocket Tasks'})
